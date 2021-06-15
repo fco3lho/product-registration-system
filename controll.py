887a7070a67268110ae97a702f14c6bf8446754f
Felipe Campos <felipecampos50123@gmail.com>
2021-06-15 11:34:07 -0300

    Lesson 2

--- controll.py
+++ controll.py
@@ -1,21 +1,39 @@
 from PyQt5 import uic, QtWidgets
+import mysql.connector
+
+banco = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    passwd = "",
+    database = "product_registration"
+)
 
 def mainFunction():
     codigo = form.lineEdit.text()
     descricao = form.lineEdit_2.text()
     preco = form.lineEdit_3.text()
+    categoria = ""
 
     print("Código:", codigo)
     print("Descrição:", descricao)
     print("Preço: R$", preco)
 
     if form.radioButton.isChecked():
-        print("Categoria: Periféricos")
+        categoria = "Periféricos"
+        print("Categoria:", categoria)
     elif form.radioButton_2.isChecked():
-        print("Categoria: Hardware")
+        categoria = "Hardware"
+        print("Categoria:", categoria)
     elif form.radioButton_3.isChecked():
-        print("Categoria: Acessórios")
+        categoria = "Acessórios"
+        print("Categoria:", categoria)
     
+    cursor = banco.cursor()
+    command_sql = "INSERT INTO produtos (codigo,descricao,preco,categoria) VALUES (%s,%s,%s,%s)"
+    data = (str(codigo), str(descricao), str(preco), categoria)
+    cursor.execute(command_sql, data)
+    banco.commit()
+
     print("")
 
 app = QtWidgets.QApplication([])
